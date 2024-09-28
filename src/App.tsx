ccfe7a4dfb5e84ce589555a16985ca83abad0f94
Bartosz Krajka <krajka.bartosz@gmail.com>
2024-09-28 14:18:09 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="app-container">
+      <div className="chat-window">
+        <div className="dialog-cloud assistant">Witam</div>
+        <div className="dialog-cloud user">Hej co tam</div>
+        <div className="dialog-cloud assistant">A nic</div>
+        <div className="dialog-cloud user">A tak o sobie piszę</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
