0497f1d78a79af91bd7ae319c751369c49b5ac73
Bartosz Krajka <krajka.bartosz@gmail.com>
2024-09-28 17:58:50 +0200

    Add remote chat functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,87 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { AzureOpenAI } from "openai";
+import './App.css';
+
+const endpoint = "https://hackathonaistu9265075225.openai.azure.com/";
+const apiVersion = "2024-02-15-preview";
+const deployment = "gpt-4"; //This must match your deployment name.
+
+const remoteRequest = async (messages: Msg[], apiKey: string): Promise<string> => {
+  const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment, dangerouslyAllowBrowser: true });
+  const remoteMessages = [
+    { role: "system", content: "Zbierasz dane do formularza PCC-3. Potrzebne dane to: PESEL, przedmiot sprzedaży, kwota sprzedaży. Dopóki nie masz wszystkich potrzebnych informacji, pytaj o szczegóły. Nie pytaj o dane które już znasz. Nie akceptuj odpowiedzi w stylu: nie wiem, nie interesuj się. Przestań rozmawiać natychmiast po uzyskaniu wszystkich potrzebnych danych i zwróć je w formacie JSON: { \"PESEL\": {PESEL}, \"object\": {przedmiot sprzedaży}, \"price\": {kwota sprzedaży} }" },
+    ...messages
+  ]
+  
+  const result = await client.chat.completions.create({
+    messages: remoteMessages
+  });
+
+  let response = ""; 
+  for (const choice of result.choices) {
+    response += choice.message.content;
+  }
+  return response;
+};
+
+type Msg = {
+  role: "user" | "assistant";
+  content: string;
+};
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [prompt, setPrompt] = useState<string>("Brother");
+  const [messages, setMessages] = useState<Msg[]>([]);
+  const [showPrompt, setShowPrompt] = useState<boolean>(true);
+  const [apiKey, setApiKey] = useState<string>("");
+
+  const displayMessages: Msg[] = [
+    { role: "assistant", content: "Witam, pomogę Ci wypełnić formularz PCC. Opisz swoją sprawę najlepiej jak umiesz." },
+     ...messages
+  ]
+
+
+  const handleChange =  (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPrompt(event.target.value);
+  }
+
+  const handleAPIKeyChange =  (event: React.ChangeEvent<HTMLInputElement>) => {
+    setApiKey(event.target.value);
+  }
+
+  const handleSend = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setShowPrompt(false);
+
+    const newMessages = [...messages];
+    newMessages.push({ role: "user", content: prompt});
+    setMessages(newMessages);
+
+    const response = await remoteRequest(messages, apiKey);
+    newMessages.push({ role: "assistant", content: response});
+    setMessages(newMessages);
+    
+    setPrompt("");
+    setShowPrompt(true);
+  }
+
+  const isButtonDisabled = prompt.length < 1;
 
   return (
     <div className="app-container">
+      Azure Open AI api key: <input value={apiKey} type="text" onChange={handleAPIKeyChange}></input>
       <div className="chat-window">
-        <div className="dialog-cloud assistant">Witam</div>
-        <div className="dialog-cloud user">Hej co tam</div>
-        <div className="dialog-cloud assistant">A nic</div>
-        <div className="dialog-cloud user">A tak o sobie piszę</div>
+        { displayMessages.map((m: Msg, index: number) => <div key={index} className={`dialog-cloud ${m.role as string}`}>{m.content}</div>) }
+        { showPrompt ?  
+          <div>
+            <input type="text" value={prompt} onChange={handleChange} />
+            <form onSubmit={handleSend} >
+              <button type="submit" disabled={isButtonDisabled}>Wyślij</button>
+            </form>
+          </div>
+          :
+          <div>Waiting...</div>
+        }
       </div>
     </div>
   )
